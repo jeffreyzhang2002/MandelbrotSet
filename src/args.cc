092b50aa157ae83ca30c6481bf06275a4a44df29
Jeffrey <jzhang3224@gatech.edu>
2025-04-05 19:29:40 -0400

    Working I think

--- src/args.cc
+++ src/args.cc
@@ -7,7 +7,6 @@ bool check_float(char* str, float& set) {
     set = strtod(str, &endptr);
             
     if(str == endptr) {
-        printf("%s is not a float\n", str);
         return false;
     }
     return true;
@@ -19,13 +18,13 @@ Args::Args() {
     this->width = 0;
     this->height = 0;
     this->step = 0;
+    this->tasks_per_thread = 0;
     this->file = std::string("");
 }
 
 bool Args::parse(int argc, char* argv[], Args& args) {
     if(argc <= 3) {
-        printf("Usage: %s --start <float> <float> --dim <float> <float> --step <int> --file <path>\n", argv[0]);
-        return 0;
+        return false;
     }
     
     int i = 1;
@@ -34,18 +33,15 @@ bool Args::parse(int argc, char* argv[], Args& args) {
     while(i < argc) {
         if(std::strcmp(argv[i], "--start") == 0) {
             if(i + 2 >= argc) {
-                printf("--start flag is missing two arguments\n");
                 return false;
             }
 
             if(!check_float(argv[i+1], args.start_x) || !check_float(argv[i+2], args.start_y)) {
-                printf("Failed to parse float for --start argument\n");
                 return false;
             }
             i += 2;
         } else if (std::strcmp(argv[i], "--dim") == 0) {
             if(i + 2 >= argc) {
-                printf("--dim flag is missing two arguments\n");
                 return false;
             }
 
@@ -55,12 +51,10 @@ bool Args::parse(int argc, char* argv[], Args& args) {
             i += 2;
         } else if (std::strcmp(argv[i], "--step") == 0) {
             if(i + 1 > argc) {
-                printf("--step flag is missing argument\n");
                 return false;
             }
             
             if(!check_float(argv[i+1], args.step)) {
-                printf("Failed to parse float for --step argument\n");
                 return false;
             }
             i += 1;
@@ -69,8 +63,11 @@ bool Args::parse(int argc, char* argv[], Args& args) {
             args.file = std::string(argv[i + 1]);
 
             i += 1;
+        } else if (std::strcmp(argv[i], "--tasks-per-thread") == 0) {
+            args.tasks_per_thread = atoi(argv[i+1]);
+
+            i += 1;
         } else {
-            printf("%s is not a valid flag\n", argv[i]);
             return false;
         }
 
@@ -78,12 +75,14 @@ bool Args::parse(int argc, char* argv[], Args& args) {
     }
 
     if(args.step <= 0 || args.width <= 0 || args.height <= 0) {
-        printf("step and dimensions must be positive values\n");
+        return false;
+    }
+
+    if(args.tasks_per_thread < 1) {
         return false;
     }
 
     if(args.file == "") {
-        printf("path must not be empty");
         return false;
     }
 
--- src/main.cu
+++ src/main.cu
@@ -5,118 +5,208 @@
 #include <fstream>
 #include "args.h"
 #include <cstdint>
+#include <mpi.h>
 
+#define DEBUG
 #ifdef DEBUG
     #define PRINT(...) printf(__VA_ARGS__)
+    #define MPI_PRINT(rank, ...) if(rank == 0) printf(__VA_ARGS__);
 #else
     #define PRINT(...)
+    #define MPI_PRINT(rank, ...)
 #endif
 
 #define MAX_ITERAION 255
-#define INT_TYPE uint8_t
+#define INT_TYPE uint8_t // Note we use a smaller int type to save on memory
 
 __global__
-void mandelbrot(INT_TYPE *p, int offset, int length, int img_width, int img_height, float start_x, float start_y, float step) {
+void mandelbrot(INT_TYPE *p, int length, int offset, int tasks_per_thread, int img_width, int img_height, float start_x, float start_y, float step) {
    
     // Thread Index 
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
    
-    // if(threadIdx.x == 0) {
-    //     printf("Block: %d index: %d\n", blockIdx.x, idx);
-    // }
 
-    // Mask out threads that are out of range
-    if(idx >= length) {
-        return;
+    for(int i = 0; i < tasks_per_thread; i++) {
+        // if(threadIdx.x == 0) {
+        //     printf("Block: %d index: %d\n", blockIdx.x, idx);
+        // }
+
+        int task_idx = idx * tasks_per_thread + i;
+
+        // Mask out threads that are out of range
+        if(task_idx >= length) {
+            return;
+        }
+
+        // Pixel Index
+        int pixel_idx = task_idx + offset;
+
+        // Convert Pixel Index to Image Coordinates
+        int img_x = pixel_idx % img_width;
+        int img_y = pixel_idx / img_width;
+
+        // Convert Image Coordinates to Cartesian Coordinates
+        int cart_x = img_x;
+        int cart_y = img_height - img_y - 1;
+
+        float real = cart_x * step + start_x;
+        float im = cart_y * step + start_y;
+        
+        float x = 0;
+        float y = 0;
+
+
+        INT_TYPE iteration = 0;
+
+        while(iteration < MAX_ITERAION && x * x + y * y <= 4) {
+            float temp = x*x - y*y + real;
+            y = 2 * x * y + im;
+            x = temp;
+            iteration++;
+        }
+
+        p[task_idx] = iteration;
     }
-
-    // Pixel Index
-    int pixel_idx = idx + offset;
-
-    // Convert Pixel Index to Image Coordinates
-    int img_x = pixel_idx % img_width;
-    int img_y = pixel_idx / img_width;
-
-    // Convert Image Coordinates to Cartesian Coordinates
-    int cart_x = img_x;
-    int cart_y = img_height - img_y - 1;
-
-    float real = cart_x * step + start_x;
-    float im = cart_y * step + start_y;
-    
-    float x = 0;
-    float y = 0;
-
-
-    INT_TYPE iteration = 0;
-
-    while(iteration < MAX_ITERAION && x * x + y * y <= 4) {
-        float temp = x*x - y*y + real;
-        y = 2 * x * y + im;
-        x = temp;
-        iteration++;
-    }
-
-    p[idx] = iteration;
 }
 
 int main(int argc, char* argv[]) {
+  
+    MPI_Init(&argc, &argv);
 
+    int world_size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+
+
+  
     Args args;
+    if(!Args::parse(argc, argv, args)) {
+        PRINT("Failed to parse args!\nExpected: %s --start <float> <float> --dim <int> <int> --step <float> --file <string> --tasks-per-thread <int>", argv[0]);
+        MPI_Finalize();
+        return 1; 
+    }
 
-    if(!Args::parse(argc, argv, args)) return 1;
+      // Total number of pixel that need to be computed 
+    int problem_size = args.width * args.height; 
+
+
+    // Print on rank 0;
+    MPI_PRINT(rank, "Calculating Mandelbrot Set!\nstart_x: %f\nstart_y: %f\nend_x: %f\nend_y: %f\ndelta: %f\nimg_width: %d\nimg_height: %d\ntasks_per_thread: %d\nProblem_size: %d\nNodes: %d\n", 
+                args.start_x, 
+                args.start_y, 
+                args.start_x + args.width * args.step, 
+                args.start_y + args.height * args.step,
+                args.step,
+                args.width,
+                args.height,
+                args.tasks_per_thread,
+                problem_size,
+                world_size
+        );
+   
+
+    // Amount of pixels designated for each node 
+    int node_length = problem_size / world_size; 
+
+    // Update the amount of work for the last node to take into account excess;
+    int remainder_length = problem_size % world_size;
+    int curr_node_length = node_length + (rank == world_size - 1? remainder_length : 0); 
     
-    fprintf(stderr, "Calculating Mandelbrot Set!\nstart_x: %f\nstart_y: %f\nend_x: %f\nend_y: %f\ndelta: %f\nwidth: %d\nheight: %d\n", 
-            args.start_x, 
-            args.start_y, 
-            args.start_x + args.width * args.step, 
-            args.start_y + args.height * args.step,
-            args.step,
-            args.width,
-            args.height
+    // Number of cuda threads requires for current node
+    int curr_node_required_threads = curr_node_length / args.tasks_per_thread + (curr_node_length % args.tasks_per_thread != 0);
+
+    // Number of blocks needed for current node
+    int curr_node_required_blocks = (curr_node_required_threads / 1024) + (curr_node_required_threads % 1024 != 0);
+
+    int curr_node_offset = rank * node_length;
+
+    PRINT("Rank %d/%d: Threads: %d Blocks: %d Tasks: %d Offset: %d\n", 
+            rank, 
+            world_size, 
+            curr_node_required_threads, 
+            curr_node_required_blocks, 
+            curr_node_length,
+            curr_node_offset
     );
-    
-    INT_TYPE *host_buffer, *device_buffer;
 
-    int length = args.width * args.height;
+    INT_TYPE *host_buffer, *device_buffer, *global_buffer;
 
     // Declare a buffer on the host which will be used to export data;
-    if(!(host_buffer = (INT_TYPE*) calloc(length, sizeof(INT_TYPE)))) {
-        fprintf(stderr, "Failed to allocate buffer on host\n");
-        return 1;
+    if(!(host_buffer = (INT_TYPE*) calloc(curr_node_length, sizeof(INT_TYPE)))) {
+        MPI_PRINT(rank, "Failed to allocate buffer on host\n");
+        MPI_Abort(MPI_COMM_WORLD, 2); 
     }
 
     // Declare a buffer on the device which will be used for computation;
-    cudaError_t cudaStatus = cudaMalloc((void**) &device_buffer, length * sizeof(INT_TYPE));
+    cudaError_t cudaStatus = cudaMalloc((void**) &device_buffer, curr_node_length * sizeof(INT_TYPE));
     if(cudaStatus != cudaSuccess) {
-        fprintf(stderr, "Failed to allocate buffer on device: %s\n", cudaGetErrorString(cudaStatus));
-        return 1;
+        MPI_PRINT(rank, "Failed to allocate buffer on device: %s\n", cudaGetErrorString(cudaStatus));
+        MPI_Abort(MPI_COMM_WORLD, 3);
     };
 
-    cudaMemset(device_buffer, 0, length);
+    // cudaMemset(device_buffer, 0, node_length * sizeof(INT_TYPE));
 
-    int numblocks = (length / 1024) + (length % 1024 == 0? 0 : 1);
-
-    fprintf(stderr, "Using %d Block with 1024 threads per block\n", numblocks);
-
-    mandelbrot<<<numblocks,1024>>>(device_buffer, 0, length, args.width, args.height, args.start_x, args.start_y, args.step);
+    
+    mandelbrot<<<curr_node_required_blocks, 1024>>>(
+        device_buffer, 
+        curr_node_length, 
+        curr_node_offset, 
+        args.tasks_per_thread, 
+        args.width, 
+        args.height, 
+        args.start_x, 
+        args.start_y, 
+        args.step
+    );
 
     cudaDeviceSynchronize();
 
-    cudaMemcpy(host_buffer, device_buffer, length * sizeof(INT_TYPE), cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_buffer, device_buffer, node_length * sizeof(INT_TYPE), cudaMemcpyDeviceToHost);
 
-    std::cerr << "Writing to " << args.file << "\n";
+    
 
-    std::ofstream file;
-    file.open(args.file);
-
-    file << "Dim: " << args.width << " " << args.height << "\n";
-
-    for(int i = 0; i < length; i++) {
-        //printf("%d = %d\n", i, host_buffer[i]);
-        file << +host_buffer[i] << "\n";
+    if(!rank) {
+        if(!(global_buffer = (INT_TYPE*) malloc(problem_size * sizeof(INT_TYPE)))) {
+            MPI_PRINT(rank, "Failed to allocate global buffer on device");
+            MPI_Abort(MPI_COMM_WORLD, 4);
+        }
     }
 
-    file.close();
-    return 0;
+    int *recvcounts = (int*) malloc(world_size * sizeof(int));
+    int *recvoffset = (int*) malloc(world_size * sizeof(int));
+    int offset = 0;
+
+    for(int i = 0; i < world_size; i++) {
+        recvcounts[i] = node_length; 
+        if(i == world_size - 1) {
+            recvcounts[i] += remainder_length;
+        }
+
+        recvoffset[i] = offset;
+        offset += node_length;
+
+        MPI_PRINT(rank, "%d offset: %d count: %d\n", i, recvoffset[i], recvcounts[i]);
+    }
+
+    MPI_Gatherv(host_buffer, curr_node_length, MPI_UINT8_T, global_buffer, recvcounts, recvoffset, MPI_UINT8_T, 0, MPI_COMM_WORLD);
+
+    if(!rank) {
+
+        PRINT("Writing to %s\n", args.file.c_str());
+
+        std::ofstream file;
+        file.open(args.file);
+
+        file << "Dim: " << args.width << " " << args.height << "\n";
+
+        for(int i = 0; i < problem_size; i++) {
+            //printf("%d = %d\n", i, host_buffer[i]);
+            file << +global_buffer[i] << "\n";
+        }
+
+        file.close();
+    }
+
+    MPI_Finalize();
 }
